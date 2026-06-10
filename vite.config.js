2ef3f9ca331bbcecd93be76f7a1bc68002efbdff
Yasnaya Merin <yassi@Yasnayas-MacBook-Air.local>
2026-06-10 14:28:36 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'final01',
+  server: {
+    port: 5500,
+    host: true
+  }
+})
+
+// export default defineConfig({
+//   build: {
+//     minify: 'esbuild',
+//   },
+//   esbuild: {
+//     // This drops all console.log calls during the production build
+//     drop: ['console', 'debugger'], 
+//   }
+// })
