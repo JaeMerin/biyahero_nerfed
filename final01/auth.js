ca6f051c756bdef5f3cdbcddedb9d94994efe8fa
Yasnaya Merin <yassi@Yasnayas-MacBook-Air.local>
2026-06-11 11:40:07 +0800

    r

--- final01/auth.js
+++ final01/auth.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (error) {
         console.error(error.message);
       } else {
-        window.location.href = `${window.location.origin}/login`;
+        window.location.href = `${window.location.origin}`;
       }
     });
   }
